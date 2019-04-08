947820daf8191fc2392b9a67d7e4d2849db98143
AleksandrHub <masterAlexandr@yandex.ru>
2019-04-08 17:00:41 +0300

    Initial commit

---
+++ desktop/src/ru/aleksandrhub/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.aleksandrhub.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.aleksandrhub.game.MadPenguin;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MadPenguin(), config);
+	}
+}
